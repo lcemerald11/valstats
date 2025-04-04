adfcc60cca4364d939e12530633c1b25129675f9
lcemerald11 <meher004@umn.edu>
2025-04-04 11:47:45 -0500

    Players
    Scrapped for player name, gamer tag, role, and country flag

--- commands/team.py
+++ commands/team.py
@@ -10,12 +10,13 @@ class TeamCommand:
         @tree.command(name="hello", description="Say hello!")
         @app_commands.describe(
             name="Team name",
-            circuit= "VCT, Game Changers.."
+            search= "Place in search"
         )
-        async def hello(interaction: discord.Interaction, name: str, circuit: str):
+        async def hello(interaction: discord.Interaction, name: str, search: int):
             results = search_teams(name)
-            print(results)
-            await interaction.response.send_message(f"Hello, " + results[0][0] + " " + results[0][1])  
+            # print(results)
+            find_players(results[search-1][1])
+            await interaction.response.send_message(f"Hello, " + results[search-1][0] + " " + results[search-1][1])  
 
 
 def search_teams(team_query):
@@ -48,3 +49,34 @@ def search_teams(team_query):
                 teams.append((name, link))
 
     return teams
+
+def find_players(url):
+    players = []
+    headers = {
+        "User-Agent": "Mozilla/5.0"  # Helps avoid being blocked
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+
+    for player_div in soup.select(".team-roster-item"):
+        game_tag = player_div.select_one(".team-roster-item-name-alias")  # Team name
+        name_tag = player_div.select_one(".team-roster-item-name-real")  # Team name
+        role = player_div.select_one(".team-roster-item-name-role")
+        flag = "flag_" + player_div.find("i")["class"][1][4:]
+        if game_tag:
+            gamer_name = game_tag.text.strip()
+        if name_tag:
+            player_name = name_tag.text.strip()
+
+        
+        if not role:
+            if not player_div.select_one(".fa-star"):
+                role_name = "player" 
+            else:
+                role_name = "IGL"
+        else:
+            role_name = role.text.strip()
+        players.append([gamer_name,player_name,role_name,flag])
+    print(players)
